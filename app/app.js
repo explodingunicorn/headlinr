15281f510d3df622fcbddb83d036eccd10b6c66f
Corey Robinson <c7o7r7e7y@gmail.com>
2017-03-23 09:29:13 -0400

    Some random leftovers

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ var sentiment = _interopDefault(require('sentiment'));
 
 // Simple wrapper exposing environment variables to rest of the code.
 
+// The variables have been written to `env.json` by the build process.
 var env = jetpack.cwd(__dirname).read('env.json', 'json');
 
 function HeadlineComment(comment, user) {
